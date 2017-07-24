3437e8bac098cc49bc5f48daf42ababfc7ed7212
Surya <itzmesurya@gmail.com>
2017-07-24 16:11:04 +0530

    daterange, expand-collapse , tooltip implementation and new reports and images

--- js/dateRangeFilterImplementation.js
+++ js/dateRangeFilterImplementation.js
@@ -2,9 +2,23 @@
 Add span with id's startDate, endDate, dateRangeText
 */
 $(document).ready(function () {
+    /** Temp fix: create id attributes out of rid */
+    var ridElements = $('[rid]');
+    for (var index = 0; index < ridElements.length; index++) {
+        var element = ridElements[index];
+        var ridVal = $(element).attr('rid');
+        $(element).attr('id', ridVal);
+    }
+    /**Temp Fix: find the first select in the searchforms */
+    var searchForms = $('[name="searchForm"]');
+    var firstSelect = $($(searchForms)[0]).find('select')[0];
+    $(firstSelect).attr('id', 'paramperiod0');
+
     /**rename paramstart_date --> paramstart_date1 (ie., add sequence) */
     seqid('paramstart_date');
     seqid('paramend_date');
+    AddDatePickersToInputStartingWith('paramstart_date', true, 'paramend_date');
+    //AddDatePickersToInputStartingWith('paramend_date');
     /** add id="paramperiod" to the select */
     var paramperiodElement = $('[name="paramperiod"]');
     if (paramperiodElement.length > 0) {
@@ -40,25 +54,84 @@ $(document).ready(function () {
     $('#paramperiod').change(function (e) {
         var selectedText = $('#paramperiod option:selected').text();
         var selectedVal = $(this).val();
+        if (selectedText.toLowerCase() === 'custom') {
+            $('#paramstart_date1').removeAttr('disabled');
+        } else {
+            $('#paramstart_date1').attr('disabled', 'disabled');
+            $('#paramend_date1').attr('disabled', 'disabled');
+        }
         setDateRange(selectedVal);
         updateStartAndEndDate(startDate, endDate, selectedText);
     });
+    /** Default values for dashboard data */
+    $('#paramperiod0').val('90');
+    $('#dateRangeTextTop').text($('#paramperiod0 option:selected').text());
+    updateStartAndEndDateForDashBoard(startDate, endDate);
     $('#paramperiod0').change(function (e) {
-        var selectedText = $('#paramperiod option:selected').text();
+        var selectedText = $('#paramperiod0 option:selected').text();
         var selectedVal = $(this).val();
+        if (selectedText.toLowerCase() === 'custom') {
+            $('#paramstart_date0').removeAttr('disabled');
+        } else {
+            $('#paramstart_date0').attr('disabled', 'disabled');
+            $('#paramend_date0').attr('disabled', 'disabled');
+        }
         setDateRange(selectedVal);
         updateStartAndEndDateForDashBoard(startDate, endDate, selectedText);
     });
     /** Create sequence id function */
     function seqid(id) {
         var paramstart_dateElements = $('[id^="' + id + '"]');
-        if (paramstart_dateElements.length > 1) {
-            for (var index = 0; index < paramstart_dateElements.length; index++) {
-                var element = paramstart_dateElements[index];
-                $(element).attr('id', id + index);
+        // if (paramstart_dateElements.length > 1) {
+        for (var index = 0; index < paramstart_dateElements.length; index++) {
+            var element = paramstart_dateElements[index];
+            $(element).attr('id', id + index);
+        }
+        // }
+    }
+    /** Datepicker implementation */
+    /**
+     * Add Datepicker widget to the input boxes
+     */
+    function AddDatePickersToInputStartingWith(id, AddChangeDateHandler, cascadeTarget) {
+        var paramstart_dateElements = $('[id^="' + id + '"]');
+        for (var index = 0; index < paramstart_dateElements.length; index++) {
+            var element = paramstart_dateElements[index];
+            if (AddChangeDateHandler) {
+                var cascadeTargetId = cascadeTarget + index;
+                // var parentId = id + index;
+                $(element).attr('cascadeTargetId', cascadeTargetId);
+                $(element).datepicker({
+                    autoclose: true,
+                    forceParse: true
+                }).on('changeDate', function (e) {
+                    var parentId = $(this).attr('id')
+                    var cascadeTargetId = $(this).attr('cascadeTargetId')
+                    AddDateCascadingFunctionality(cascadeTargetId, parentId);
+                });
+
+            } else {
+                $(element).datepicker({
+                    autoclose: true,
+                    forceParse: true
+                });
             }
         }
     }
+
+    function AddDateCascadingFunctionality(id, parentId) {
+        $('#' + id).val($('#' + parentId).val());
+        $('#' + id).removeAttr('disabled');
+        /** assign a datepicker and set the start date 
+         * from the parentId
+         */
+        var parentIdDate = $('#' + parentId).datepicker('getDate');
+        $('#' + id).datepicker({
+            autoclose: true,
+            startDate: parentIdDate
+        });
+    }
+
 
     /** Moving swith for date range into a function */
     function setDateRange(selectedVal) {
@@ -141,28 +214,39 @@ $(document).ready(function () {
     }
 
     $('#apply1').click(function (e) {
+        $(this).closest('.dropdown-menu').find('.close').click();
         applyStartAndEndDate(startDate, endDate, $('#paramperiod option:selected').text());
+    });
+
+    $('#apply0').click(function (e) {
+        $(this).closest('.dropdown-menu').find('.close').click();
+        applyStartAndEndDateForDashBoard($('#paramperiod0 option:selected').text());
     });
 });
 
 
 function updateStartAndEndDate(startDate, endDate, dateRangeText) {
     $('#paramstart_date1').val(startDate);
-    if ($('[id^="paramend_date"]').length > 1) {
-        $('#paramend_date1').val(endDate);
-    } else {
-        $($('[id^="paramend_date"]')[0]).attr('id','paramend_date');
-        $('#paramend_date').val(endDate);
-    }
-
+    $('#paramend_date1').val(endDate);
+    // if ($('[id^="paramend_date"]').length > 1) {
+    //     $('#paramend_date0').val(endDate);
+    // } else {
+    //     //$($('[id^="paramend_date"]')[0]).attr('id','paramend_date');
+    //     $('#paramend_date0').val(endDate);
+    // }
 }
 
-function updateStartAndEndDateForDashBoard(startDate, endDate, dateRangeText) {
-     $('#dateRangeText').text(dateRangeText);
+function updateStartAndEndDateForDashBoard(startDate, endDate) {
+    $('#paramstart_date0').val(startDate);
+    $('#paramend_date0').val(endDate);
 }
 
 function applyStartAndEndDate(startDate, endDate, dateRangeText) {
     $('#dateRangeText').text(dateRangeText);
     $('#startDate').text(startDate);
     $('#endDate').text(endDate);
+}
+
+function applyStartAndEndDateForDashBoard(dateRangeText) {
+    $('#dateRangeTextTop').text(dateRangeText);
 }
